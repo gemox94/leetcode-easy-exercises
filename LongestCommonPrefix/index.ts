a88a1d1bb59ba3607cb09d03f55bca86a62530ce
Gerardo Moxca Ruiz <gerardo.mx.rz@gmail.com>
2020-08-27 00:32:20 -0500

    added more testcases, updated console log

--- LongestCommonPrefix/index.ts
+++ LongestCommonPrefix/index.ts
@@ -71,10 +71,34 @@ function longestCommonPrefixV2(words: string[]): string {
     for (let i=1; i<words.length; i++) {
 
         /**
+         * Here, we get the word at index 'i' and ask if it contains in some way the prefix.
+         * Remember that the first value of the prefix is the first word in the array.
+         * So we can understand how this works, we have to know how indexOf works.
+         * It basically looks in an array the index where the passed word matches against the one we're comparing
+         * For example, if we have a phrase like "Is Awesome" and we try to find "Awesome" in that phrase, we can use
+         * indexOf -> "Is Awesome".indexOf("Awesome") will return 3 becase is the index where the word "Awesome" begins.
          *
+         * So with that in mind, what we're trying here is, when the prefix matches the index 0 of the comparing word,
+         * that way we're sure is a prefix because is at the beginning of the word.
          */
         while (words[i].indexOf(prefix) !== 0) {
+
+            /**
+             * Here, we're re-assigning the prefix by deleting the last word of it.
+             * That way, we're removing the letters that are not from the prefix, for example:
+             * The idea of the while and the prefix.substring is, for example, we have:
+             * "plastic" as the word[i] and "play" as the prefix.
+             *
+             * The first time the while runs, it's going to check if "plastic".substring("play"), it will return -1
+             * So, we have to reduce the word "play", so, that's what we do with .substring. At the first iteration
+             * prefix.length - 1 = 3 (4-1) we do "play".substring(0, 3), so what we're doing is re-assigning the prefix
+             * prefix = "pla".
+             * As we can see, the while will stops until prefix will be "pl" it's when the words[i].indexOf(prefix)
+             * will throw 0 and the while will stops.
+             */
             prefix = prefix.substring(0, prefix.length - 1);
+
+            // At the end if the prefix gets empty, it means it never matched the word[i], so we return empty string
             if (prefix === '') return '';
         }
     }
@@ -84,5 +108,5 @@ function longestCommonPrefixV2(words: string[]): string {
 
 testcases.forEach((testcase: string[]) => {
     const result = longestCommonPrefixV2(testcase);
-    console.log('LCP: ', result);
+    console.log(`Testcase: ${testcase} - LCP: ${result}`);
 });
--- LongestCommonPrefix/testcases.ts
+++ LongestCommonPrefix/testcases.ts
@@ -1,5 +1,8 @@
 export default [
     ['a', 'a', 'b'],
     ['play', 'plenty', 'plastic'],
-    ['flight', 'flower', 'flat']
+    ['flight', 'flower', 'flat'],
+    ['lovely', 'lonely', 'narrow', 'narwhal', 'narration'],
+    ['narrow', 'lovely', 'lonely', 'narwhal', 'narration'],
+    ['asymmetric', 'asymptomatic', 'asylum']
 ]
